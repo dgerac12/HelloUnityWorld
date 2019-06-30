75d5d8579f785f44d4997c0e1f173a77a0d9e37f
Danielle Geraci <daniellegeraci@knights.ucf.edu>
2019-06-30 09:39:21 -0400

    First commit

---
+++ Assets/ChangeName.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChangeName : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+            textmeshPro.SetText("Hello World from Danielle Geraci");
+        }
+    }
+}
